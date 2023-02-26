3ef123df625ad272a5558e519d321b8c204e484e
Naomi Downing <downing3@bu.edu>
2023-02-26 09:43:46 -0500

    fixed font colors

--- src/components/HPButtons.jsx
+++ src/components/HPButtons.jsx
@@ -4,11 +4,11 @@ function HPButtons() {
   return (
     <div className="flex justify-center">
     <div className="flex flex-col space-y-3">
-      <button className="bg-buttons hover:bg-buttons text-white font-outfit py-2 px-10 rounded-full">
+      <button className="bg-buttons hover:bg-buttons text-dm-blue font-outfit py-2 px-10 rounded-full">
       POST ITEM
     </button>
       
-    <button className="bg-buttons hover:bg-buttons text-white font-outfit py-2 px-10 rounded-full">
+    <button className="bg-buttons hover:bg-buttons text-dm-blue font-outfit py-2 px-10 rounded-full">
       VIEW ITEMS
     </button>
 
