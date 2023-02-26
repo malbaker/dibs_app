9f08f3d9eba6fc94ee26ceced3e7928bf7451b12
hayamaj <hayamaj@bu.edu>
2023-02-26 10:07:53 -0500

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
     <label for="my-drawer" class="btn btn-ghost btn-circle">
     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
     </label>
-    
+
         <li><a>post</a></li>
         <li><a>view</a></li>
         <li><a>faqs</a></li>
