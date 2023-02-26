c42aa9c6ed9ef38319b7c0a9b91f625ce6477c43
hayamaj <hayamaj@bu.edu>
2023-02-26 09:55:23 -0500

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState, useEffect } from 'react'
 import { db } from './config/firebase'
 import { getDocs,collection, addDoc } from  'firebase/firestore'
-import Hero from './components/PromoHero'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
-    <div className="App">
-      <h1 className='text-xl text-white text-center my-2 mx-auto'> Home page</h1>
+    <div className="bg-lm-blue min-h-screen">
+      <Navbar />
     </div>
   )
 }
