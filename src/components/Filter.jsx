0a4ed705739b33a398f2fc8c0d1154dda2b6e253
Marielle <mbelomme@bu.edu>
2023-03-12 08:26:07 -0400

    Added filter button that shows a dropdown of search filters and clicking out of dropdown closes it.

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,5 +1,36 @@
+import React, { useState } from 'react';
+
 function Filter() {
+    const [showDropdown, setShowDropdown] = useState(false)
+    const onClick = () => setShowDropdown(!showDropdown)
+
+    document.addEventListener('click', (e) => {
+        if (showDropdown && !document.getElementById('filterDropdown').contains(e.target) && !document.getElementById('filterButton').contains(e.target) ) {
+            setShowDropdown(false)
+        }
+    })
     
+    return (       
+        <div className="flex">
+            <button onClick={onClick} id="filterButton" className="btn mt-2 bg-buttons hover:bg-buttons rounded-full border-transparent focus:border-transparent focus:ring-0">
+                Filter
+            </button>
+
+            <div id="filterDropdown">
+                { showDropdown ? <FilterDropdown/> : null}
+            </div>
+        </div>
+
+    )
+}
+
+function FilterDropdown() {
+    return (
+        <div>
+        <p> dropdown works
+        </p>
+        </div>
+    )
 }
 
 export default Filter
