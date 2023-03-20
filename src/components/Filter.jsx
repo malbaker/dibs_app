244aea4e1c3102bf44bf15dae00d7774e9a6a629
Marielle <mbelomme@bu.edu>
2023-03-19 20:57:32 -0400

    Cleans code.

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -12,7 +12,7 @@ function Filter({filter, setFilter, data, setPosts}) {
     
     return (       
         <div className="flex flex-col">
-            <button onClick={onClick} id="filterButton" className="btn mt-2 bg-buttons hover:bg-buttons rounded-full border-transparent focus:border-transparent focus:ring-0">
+            <button onClick={() => onClick} id="filterButton" className="btn mt-2 bg-buttons hover:bg-buttons rounded-full border-transparent focus:border-transparent focus:ring-0">
                 Filter
             </button>
 
@@ -24,7 +24,8 @@ function Filter({filter, setFilter, data, setPosts}) {
 }
 
 function FilterDropdown({filter, setFilter, data, setPosts}) {
-    const itemTypeOptions = {
+    // Filter options
+    const options = {
         category: [
             {"index": 0, "name": "furniture"}, 
             {"index": 1, "name": "home decor"}, 
@@ -39,15 +40,15 @@ function FilterDropdown({filter, setFilter, data, setPosts}) {
     }
 
     // State that will keep track of which checkboxes are checked using boolean 
-    const [selectedItemType, setSelectedItemType] = useState(() => {
+    const [selected, setSelected] = useState(() => {
         let temp = {}
-        Object.keys(itemTypeOptions).forEach((key) => {
-            temp[key] = new Array(itemTypeOptions[key].length).fill(false)
+        Object.keys(options).forEach((key) => {
+            temp[key] = new Array(options[key].length).fill(false)
 
             // In case you close dropdown and reopen it, this ensures the dropdown looks the same way you left it
-            for (const itemTypeOption of itemTypeOptions[key]) {
-                if (filter[key] && filter[key].indexOf(itemTypeOption["name"]) >= 0) {
-                    temp[key][itemTypeOption["index"]] = true
+            for (const option of options[key]) {
+                if (filter[key] && filter[key].indexOf(option["name"]) >= 0) {
+                    temp[key][option["index"]] = true
                 }
             }
         })
@@ -55,14 +56,14 @@ function FilterDropdown({filter, setFilter, data, setPosts}) {
     })
 
     const handleOnChange = (key, index) => {
-        setSelectedItemType((prevSelectedItemType) => {
-            let newSelectedItemType = {}
-            Object.keys(prevSelectedItemType).forEach((key) => {
-                newSelectedItemType[key] = [...prevSelectedItemType[key]]  
+        setSelected((prevSelected) => {
+            let newSelected = {}
+            Object.keys(prevSelected).forEach((key) => {
+                newSelected[key] = [...prevSelected[key]]  
             })
 
             // Records whether checkbox was selected/unselected
-            newSelectedItemType[key][index] = !prevSelectedItemType[key][index]
+            newSelected[key][index] = !prevSelected[key][index]
             
             // Updates the filter criteria based on selected items
             setFilter(prevFilter => {
@@ -71,14 +72,14 @@ function FilterDropdown({filter, setFilter, data, setPosts}) {
                     newFilter[key] = [...prevFilter[key]]  
                 })
                 
-                if (newSelectedItemType[key][index]) {
-                    if (key in prevFilter && prevFilter[key].indexOf(itemTypeOptions[key][index].name) < 0) {
-                        newFilter[key].push(itemTypeOptions[key][index].name)
+                if (newSelected[key][index]) {
+                    if (key in prevFilter && prevFilter[key].indexOf(options[key][index].name) < 0) {
+                        newFilter[key].push(options[key][index].name)
                     } else if (!(key in prevFilter)) {
-                        newFilter[key] = [itemTypeOptions[key][index].name]
+                        newFilter[key] = [options[key][index].name]
                     }
                 } else if (newFilter[key]) {
-                    const indexToRemove = newFilter[key].indexOf(itemTypeOptions[key][index].name);
+                    const indexToRemove = newFilter[key].indexOf(options[key][index].name);
 
                     if (indexToRemove >= 0) { 
                         newFilter[key].splice(indexToRemove, 1);
@@ -95,31 +96,30 @@ function FilterDropdown({filter, setFilter, data, setPosts}) {
                         }
                         return true
                     })
-                    console.log(newFilter)
-                    console.log(posts)
+
                     return posts
                 })
 
                 return newFilter
             })
 
-            return newSelectedItemType
+            return newSelected
         })
     }
 
-    let itemTypeInput = {}
-    Object.keys(itemTypeOptions).forEach((key) => {
-        itemTypeInput[key] = []
+    let inputs = {}
+    Object.keys(options).forEach((key) => {
+        inputs[key] = []
     
-        for (let itemType of itemTypeOptions[key]){
-            itemTypeInput[key].push(
+        for (let itemType of options[key]){
+            inputs[key].push(
                 <div key={`div-${itemType.index}`}>
                     <input className='flex-direction-row'
                         type="checkbox"
                         id={`checkbox-${itemType.index}`}
                         name={itemType.name}
                         value={itemType.name}
-                        checked={selectedItemType[key][itemType.index]}
+                        checked={selected[key][itemType.index]}
                         onChange={() => handleOnChange(key, itemType.index)}
                     />
                     <label htmlFor={`custom-checkbox-${itemType.index}`} key={`label-${itemType.index}`}>{itemType.name}</label>
@@ -131,10 +131,10 @@ function FilterDropdown({filter, setFilter, data, setPosts}) {
     return (
         <div className='flex flex-col' style={{backgroundColor: 'rgb(240 197 82 / var(--tw-bg-opacity))'}}>
             <h3>Item Type:</h3>
-            {itemTypeInput["category"]}
+            {inputs["category"]}
 
             <h3>Item Condition:</h3>
-            {itemTypeInput["condition"]}
+            {inputs["condition"]}
         </div>
     )
 }
