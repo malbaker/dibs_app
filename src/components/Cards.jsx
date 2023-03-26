cc9e684f205a8eb85e65181ce93d19b341d8f9ab
hayamaj <hayamaj@bu.edu>
2023-03-26 13:21:58 -0400

    added a modal pop-up to each card

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { db, storage } from "../config/firebase";
 import { doc, deleteDoc } from "firebase/firestore";
 import { ref, deleteObject } from "firebase/storage";
@@ -33,31 +33,56 @@ async function claimItem(post) {
 }
 
 function Card({ post }) {
+  const [showModal, setShowModal] = useState(false); // state to track whether the modal should be shown
+
+  const handleClaimItem = async () => {
+    // handle claim item logic
+    setShowModal(false); // hide the modal after claim item is successful
+  };
+
   return (
-    <div
-      className="card card-compact w-96 bg-base-100 shadow-xl text-dm-blue"
-      style={{ margin: "20px" }}
-    >
-      <figure>
-        <img
-          src={post.image}
-          alt={post.description}
-          style={{ maxHeight: "40vh", width: "100vh" }}
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{post.category}</h2>
-        <p className="card-actions justify-beginning">{post.address}</p>
-        <div className="card-actions justify-end">
+    <>
+      <div
+        className="card card-compact w-96 bg-base-100 shadow-xl text-dm-blue cursor-pointer"
+        style={{ margin: "20px" }}
+        onClick={() => setShowModal(true)} // show the modal when the card is clicked
+      >
+        <figure>
+          <img
+            src={post.image}
+            alt={post.description}
+            style={{ maxHeight: "40vh", width: "100vh" }}
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{post.category}</h2>
+          <p className="card-actions justify-beginning">{post.address}</p>
+          <div className="card-actions justify-end">
+            <label
+              htmlFor={`modal-${post.id}`}
+              className="btn mt-1 lowercase text-dm-blue bg-buttons hover:bg-buttons rounded-full border-transparent focus:border-transparent focus:ring-0"
+            >
+              more info
+            </label>
+          </div>
+        </div>
+      </div>
+      <input type="checkbox" id={`modal-${post.id}`} className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box relative">
+          <label htmlFor={`modal-${post.id}`} className="btn btn-sm btn-circle absolute right-2 top-2">
+            ✕
+          </label>
+          <h3 className="text-lg font-bold">{post.description}</h3>
           <button
             onClick={() => claimItem(post)}
-            className="btn mt-1 text-dm-blue bg-buttons hover:bg-buttons rounded-full border-transparent focus:border-transparent focus:ring-0"
+            className="btn mt-1 lowercase text-dm-blue bg-buttons hover:bg-buttons rounded-full border-transparent focus:border-transparent focus:ring-0"
           >
             claim item
           </button>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
@@ -68,4 +93,5 @@ Cards.propTypes = {
 Card.propTypes = {
   post: PropTypes.object,
 };
+
 export default Cards;
