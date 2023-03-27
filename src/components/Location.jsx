4b2d21b590d0c30bc0f1b290c7ec72491924ae57
Marielle <mbelomme@bu.edu>
2023-03-26 20:56:23 -0400

    Creates a function that uses Google's geocoding api to reverse lookup a location based on the device's coordinates

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,16 +1,37 @@
-const url = `https://www.googleapis.com/geolocation/v1/geolocate?key=${import.meta.env.VITE_FIREBASE_APIKEY}`
+// Returns coordinate points of device
+async function getCoordinates() {
+  const uri = `https://www.googleapis.com/geolocation/v1/geolocate?key=${
+    import.meta.env.VITE_FIREBASE_APIKEY
+  }`;
+  let response = await fetch(uri, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
 
-export default async function getCoordinates() {
-    let response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
-    });
+  const data = await response.json();
+  return data.location;
+}
 
-  const data = await response.json()
+// Uses reverse geocoding to lookup an address based on coordinates
+export default async function getLocation(coordinates) {
+  coordinates = coordinates || (await getCoordinates());
 
-  console.log(data.location)
-  return data.location
+  const uri = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${
+    coordinates.lat
+  },${coordinates.lng}&key=${import.meta.env.VITE_FIREBASE_APIKEY}`;
+  let response = await fetch(uri, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+    },
+  });
+
+  const data = await response.json();
+
+  return Object.assign({}, coordinates, {
+    formated_address: data.results[0].formatted_address,
+  });
 }
