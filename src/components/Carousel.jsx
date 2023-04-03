fa8206ba6fc62b8f5b3d746a1d28138a8692e986
Malik Baker <78194321+malbaker@users.noreply.github.com>
2023-04-03 14:21:16 -0400

    linting

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,7 +7,11 @@ const Carousel = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const q = query(collection(db, "posts"), orderBy("timeadded", "desc"), limit(7));
+      const q = query(
+        collection(db, "posts"),
+        orderBy("timeadded", "desc"),
+        limit(7),
+      );
       const querySnapshot = await getDocs(q);
       setData(() => {
         let data = querySnapshot.docs.map((doc) => {
