0221205cc1bf39049734f7e755684c828235bce8
hayamaj <hayamaj@bu.edu>
2023-04-16 15:32:22 -0400

    save a user's fav posts

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 import { db, auth } from "../config/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -16,35 +16,61 @@ import {
   limit,
 } from "firebase/firestore";
 
-const LikeButton = ({ post }) => {
+const LikeButton = ({ post, onLikeStatusChanged }) => {
   const [user] = useAuthState(auth);
   const [liked, setLiked] = useState(false);
   const [likesCount, setLikesCount] = useState(post.likes);
 
-  (async () => {
-    const q = query(collection(db, "users"), where("uid", "==", user.uid), limit(1));
-    const users = await getDocs(q);
+  useEffect(() => {
+    const fetchLikedStatus = async () => {
+      if (user) {
+        const q = query(
+          collection(db, "users"),
+          where("uid", "==", user.uid),
+          limit(1),
+        );
+        const users = await getDocs(q);
 
-    const userRef = doc(db, "users", users.docs[0].id);
-    setLiked((await getDoc(userRef)).data().myFavorites.includes(post.id));
-  })();
+        if (users.docs[0]) {
+          const userRef = doc(db, "users", users.docs[0].id);
+          setLiked((await getDoc(userRef)).data().myFavorites.includes(post.id));
+        }
+      }
+    };
+
+    fetchLikedStatus();
+  }, [user, post.id]);
 
   const handleLike = async () => {
-    const postRef = doc(db, "posts", post.id);
-    liked
-      ? await updateDoc(postRef, { likes: post.likes - 1 })
-      : await updateDoc(postRef, { likes: post.likes + 1 });
+    if (user && post.id) {
+      console.log(post.id); // Check if post.id is defined
+      const postRef = doc(db, "posts", post.id);
+      liked
+        ? await updateDoc(postRef, { likes: post.likes - 1 })
+        : await updateDoc(postRef, { likes: post.likes + 1 });
 
-    setLikesCount(liked ? likesCount - 1 : likesCount + 1);
+      setLikesCount(liked ? likesCount - 1 : likesCount + 1);
 
-    const q = query(collection(db, "users"), where("uid", "==", user.uid), limit(1));
-    const users = await getDocs(q);
+      const q = query(
+        collection(db, "users"),
+        where("uid", "==", user.uid),
+        limit(1),
+      );
+      const users = await getDocs(q);
 
-    const userRef = doc(db, "users", users.docs[0].id);
-    await updateDoc(userRef, {
-      myFavorites: liked ? arrayRemove(post.id) : arrayUnion(post.id),
-    });
-    setLiked(!liked);
+      if (users.docs[0]) {
+        const userRef = doc(db, "users", users.docs[0].id);
+        await updateDoc(userRef, {
+          myFavorites: liked ? arrayRemove(post.id) : arrayUnion(post.id),
+        });
+        setLiked(!liked);
+
+        // Notify the parent component about the change
+        if (onLikeStatusChanged) {
+          onLikeStatusChanged(!liked);
+        }
+      }
+    }
   };
 
   return (
@@ -61,5 +87,6 @@ const LikeButton = ({ post }) => {
 
 LikeButton.propTypes = {
   post: PropTypes.object,
+  onLikeStatusChanged: PropTypes.func,
 };
 export default LikeButton;
